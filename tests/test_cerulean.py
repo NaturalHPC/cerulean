d1fd84cda683815e107b621eb7706066fa567b5d
Lourens Veen <l.veen@esciencecenter.nl>
2018-10-12 10:47:38 +0200

    Stream test output to keep Travis awake

--- tests/test_cerulean.py
+++ tests/test_cerulean.py
@@ -2,29 +2,26 @@ import io
 import os
 import pytest
 import sh
+import sys
 import tempfile
 
 
-def test_cerulean():
-    """Runs the integration tests."""
+@pytest.fixture
+def target_images():
     workdir = os.path.dirname(__file__)
 
     # Build cerulean-test-base first, fixes dependency issue
-    output_buffer = io.StringIO()
-    sh.docker.build('-t', 'cerulean-test-base', 'container-base', _cwd=workdir, _out=output_buffer)
-    print(output_buffer.getvalue())
-    output_buffer.close()
+    sh.docker.build('-t', 'cerulean-test-base', 'container-base', _cwd=workdir, _out=sys.stdout)
 
-    output_buffer = io.StringIO()
-    sh.docker_compose.build(_cwd=workdir, _out=output_buffer)
-    print(output_buffer.getvalue())
-    output_buffer.close()
+    sh.docker_compose.build(_cwd=workdir, _out=sys.stdout)
+
+
+def test_cerulean(target_images):
+    """Runs the integration tests."""
+    workdir = os.path.dirname(__file__)
 
     # Run tests
-    output_buffer = io.StringIO()
-    sh.docker_compose.up('cerulean-test', _cwd=workdir, _out=output_buffer)
-    print(output_buffer.getvalue())
-    output_buffer.close()
+    sh.docker_compose.up('cerulean-test', _cwd=workdir, _out=sys.stdout)
 
     # Get results
     handle, path = tempfile.mkstemp()
@@ -32,14 +29,12 @@ def test_cerulean():
     sh.docker.cp('cerulean-test:/home/cerulean/pytest_exit_codes', path)
     with open(path, 'r') as exit_code_file:
         lines = exit_code_file.readlines()
-        exit_code_1 = lines[0].strip()
-        assert exit_code_1 == '0'
-
-        exit_code_2 = lines[1].strip()
-        assert exit_code_2 == '0'
 
     # Clean up target containers
-    output_buffer = io.StringIO()
-    sh.docker_compose.down(_cwd=workdir, _out=output_buffer)
-    print(output_buffer.getvalue())
-    output_buffer.close()
+    sh.docker_compose.down(_cwd=workdir, _out=sys.stdout)
+
+    exit_code_1 = lines[0].strip()
+    assert exit_code_1 == '0'
+
+    exit_code_2 = lines[1].strip()
+    assert exit_code_2 == '0'
