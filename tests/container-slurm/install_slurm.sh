515da1f79ba9004b45969875840a47bbf76a2ea1
Lourens Veen <l.veen@esciencecenter.nl>
2018-10-12 11:56:38 +0200

    Reduce verbosity of Slurm builds in test Dockers

--- tests/container-slurm/install_slurm.sh
+++ tests/container-slurm/install_slurm.sh
@@ -11,15 +11,18 @@ apt-get --no-install-recommends install -y gcc make libssl-dev libmunge-dev tar 
 
 NAME=`basename -s .tar.gz $1`
 
-wget https://github.com/SchedMD/slurm/archive/$1
-tar -xvzf $1
+wget -nv https://github.com/SchedMD/slurm/archive/$1
+tar -xzf $1
 
 cd /usr/local/slurm-$NAME
 patch -p0 </usr/local/etc/slurm_timeout.diff
 
-./configure --prefix=/usr/local --sysconfdir=/usr/local/etc/slurm
-make
-make install
+./configure --prefix=/usr/local --sysconfdir=/usr/local/etc/slurm >/tmp/configure.log 2>&1
+tail -n 20 /tmp/configure.log
+make >/tmp/make.log 2>&1
+tail -n 20 /tmp/make.log
+make install >/tmp/make_install.log 2&>1
+tail -n 20 /tmp/make_install.log
 
 cd /usr/local
 rm -rf /usr/local/slurm-$NAME
@@ -27,5 +30,5 @@ rm /usr/local/$1
 
 # NOTE: removing tar seems to break stuff.
 apt-get purge -y gcc make wget libssl-dev libmunge-dev
-apt autoremove -y
+apt-get autoremove -y
 apt-get clean -y
