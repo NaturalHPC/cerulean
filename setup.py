9dba55b5e8853bbb79a36d78788e1cf31b75b5a8
Lourens Veen <l.veen@esciencecenter.nl>
2018-11-22 22:56:20 +0100

    Set release version to 0.3.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='cerulean',
-    version='0.0.0.dev0',
+    version='0.3.1',
     description="A Python 3 library for talking to HPC clusters and supercomputers",
     long_description=readme + '\n\n',
     author="Lourens Veen",
