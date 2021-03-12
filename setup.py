1f132be49f26deef089df061b9c5d32ffcbe1afd
Lourens Veen <l.veen@esciencecenter.nl>
2021-03-12 19:42:21 +0100

    Update metadata

--- setup.py
+++ setup.py
@@ -24,14 +24,16 @@ setup(
     zip_safe=False,
     keywords='cerulean',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     test_suite='tests',
     install_requires=[
