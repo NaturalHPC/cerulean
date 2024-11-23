c6fb342634187731be369d35fb9c28c27dd34b52
Lourens Veen <l.veen@esciencecenter.nl>
2024-11-23 09:53:59 +0100

    Make flaky tests more reliable

--- cerulean/ssh_terminal.py
+++ cerulean/ssh_terminal.py
@@ -73,8 +73,15 @@ class SshTerminal(Terminal):
             An SFTP client object using this terminal's connection.
 
         """
-        self.__transport = self.__ensure_connection(self.__transport)
-        client = paramiko.SFTPClient.from_transport(self.__transport)
+        tries = 0
+        while tries < 3:
+            try:
+                self.__transport = self.__ensure_connection(self.__transport)
+                client = paramiko.SFTPClient.from_transport(self.__transport)
+                break
+            except paramiko.ssh_exception.SSHException as e:
+                tries += 1
+
         if client is None:
             raise RuntimeError('Could not open a channel for SFTP')
         return client
@@ -175,6 +182,8 @@ class SshTerminal(Terminal):
                 data.extend(new_data)
                 new_data = receive(channel, 1024 * 1024)
         except socket.timeout:
+            return False, data.decode('utf-8')
+        except ConnectionError:
             return False, data.decode('utf-8')
 
         return True, data.decode('utf-8')
--- cerulean/test/test_terminal.py
+++ cerulean/test/test_terminal.py
@@ -2,61 +2,94 @@ from pathlib import Path
 
 from cerulean import Terminal
 
+from cerulean.test.conftest import abort_on_network_error, NUM_TRIES
+from paramiko.ssh_exception import SSHException
+
 
 def test_terminal(terminal: Terminal) -> None:
-    exit_code, output, error = terminal.run(
-            10.0,
-            'echo', ['hello', 'world'])
-    assert exit_code == 0
-    assert output == 'hello world\n'
-    assert error == ''
+    tries = 0
+    while tries < NUM_TRIES:
+        with abort_on_network_error():
+            exit_code, output, error = terminal.run(
+                    10.0,
+                    'echo', ['hello', 'world'])
+            assert exit_code == 0
+            assert output == 'hello world\n'
+            assert error == ''
+            break
+        tries += 1
 
 
 def test_terminal_path(terminal: Terminal) -> None:
-    exit_code, output, error = terminal.run(
-            10.0,
-            Path('/bin/echo'), ['hello', 'world'])
-    assert exit_code == 0
-    assert output == 'hello world\n'
-    assert error == ''
+    tries = 0
+    while tries < NUM_TRIES:
+        with abort_on_network_error():
+            exit_code, output, error = terminal.run(
+                    10.0,
+                    Path('/bin/echo'), ['hello', 'world'])
+            assert exit_code == 0
+            assert output == 'hello world\n'
+            assert error == ''
+            break
+        tries += 1
 
 
 def test_terminal_stdin(terminal: Terminal) -> None:
-    exit_code, output, error = terminal.run(
-            10.0,
-            'cat', [],
-            'hello world\n')
-    assert exit_code == 0
-    assert output == 'hello world\n'
-    assert error == ''
+    tries = 0
+    while tries < NUM_TRIES:
+        with abort_on_network_error():
+            exit_code, output, error = terminal.run(
+                    10.0,
+                    'cat', [],
+                    'hello world\n')
+            assert exit_code == 0
+            assert output == 'hello world\n'
+            assert error == ''
+            break
+        tries += 1
 
 
 def test_terminal_workdir(terminal: Terminal) -> None:
-    exit_code, output, error = terminal.run(
-            10.0,
-            'bash', ['-c', 'pwd'],
-            None,
-            '/home/cerulean/test_files')
-    assert exit_code == 0
-    assert output == '/home/cerulean/test_files\n'
-    assert error == ''
+    tries = 0
+    while tries < NUM_TRIES:
+        with abort_on_network_error():
+            exit_code, output, error = terminal.run(
+                    10.0,
+                    'bash', ['-c', 'pwd'],
+                    None,
+                    '/home')
+            assert exit_code == 0
+            assert output == '/home\n'
+            assert error == ''
+            break
+        tries += 1
 
 
 def test_terminal_workdir_path(terminal: Terminal) -> None:
-    exit_code, output, error = terminal.run(
-            10.0,
-            'bash', ['-c', 'pwd'],
-            None,
-            Path('/home/cerulean/test_files'))
-    assert exit_code == 0
-    assert output == '/home/cerulean/test_files\n'
-    assert error == ''
+    tries = 0
+    while tries < NUM_TRIES:
+        with abort_on_network_error():
+            exit_code, output, error = terminal.run(
+                    10.0,
+                    'bash', ['-c', 'pwd'],
+                    None,
+                    Path('/home'))
+            assert exit_code == 0
+            assert output == '/home\n'
+            assert error == ''
+            break
+        tries += 1
 
 
 def test_running_in_shell(terminal: Terminal) -> None:
-    exit_code, output, error = terminal.run(
-            10.0,
-            'export CERULEAN=cerulean ; echo $CERULEAN', [])
-    assert exit_code == 0
-    assert output == 'cerulean\n'
-    assert error == ''
+    tries = 0
+    while tries < NUM_TRIES:
+        with abort_on_network_error():
+            exit_code, output, error = terminal.run(
+                    10.0,
+                    'export CERULEAN=cerulean ; echo $CERULEAN', [])
+            assert exit_code == 0
+            assert output == 'cerulean\n'
+            assert error == ''
+            break
+        tries += 1
